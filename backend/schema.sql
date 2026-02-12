97700e52a8948cdae1143422a4a1001450647767
Tom <157160253+txlney@users.noreply.github.com>
2026-02-12 01:38:17 +0000

    Added airports and airlines tables with seeding from AirLabs

--- backend/schema.sql
+++ backend/schema.sql
@@ -11,15 +11,33 @@ CREATE TABLE IF NOT EXISTS users (
     -- add constraints for email check
 );
 
+-- Airports
+CREATE TABLE IF NOT EXISTS airports (
+    port_iata TEXT PRIMARY KEY,
+    port_icao TEXT,
+    port_name TEXT NOT NULL,
+    port_city TEXT,
+    port_country_code TEXT
+);
+
+-- Airlines
+CREATE TABLE IF NOT EXISTS airlines (
+    airline_icao TEXT PRIMARY KEY,
+    airline_iata TEXT,
+    airline_name TEXT NOT NULL,
+    airline_country_code TEXT
+);
+
 -- Aircraft
 CREATE TABLE IF NOT EXISTS aircraft (
     air_icao24_hex TEXT PRIMARY KEY,
     air_reg TEXT UNIQUE,
-    air_airline TEXT,
+    air_airline_icao TEXT,
     air_icao_type TEXT,
     air_model_name TEXT,
     air_manufacturer TEXT,
-    air_age INT
+    air_age INT,
+    FOREIGN KEY (air_airline_icao) REFERENCES airlines(airline_icao)
 );
 
 -- Sighting Logs
@@ -28,12 +46,14 @@ CREATE TABLE IF NOT EXISTS logs (
     user_id INT NOT NULL,
     air_icao24_hex TEXT NOT NULL,
     log_timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
-    log_dep_port TEXT,
-    log_arr_port TEXT,
+    log_dep_iata TEXT,
+    log_arr_iata TEXT,
     log_latitude REAL,
     log_longitude REAL,
     log_altitude INT,
     log_notes TEXT,
     FOREIGN KEY (user_id) REFERENCES users(user_id),
-    FOREIGN KEY (air_icao24_hex) REFERENCES aircraft(air_icao24_hex)
+    FOREIGN KEY (air_icao24_hex) REFERENCES aircraft(air_icao24_hex),
+    FOREIGN KEY (log_dep_iata) REFERENCES airports(port_iata),
+    FOREIGN KEY (log_arr_iata) REFERENCES airports(port_iata)
 );
